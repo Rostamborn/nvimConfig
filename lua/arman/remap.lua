c6428bc7762bca60c48290b535cbb536e27e03bf
Arman <rostamborne@gmail.com>
2023-05-07 14:16:28 +0330

    add map for no highlight

--- lua/arman/remap.lua
+++ lua/arman/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<C-S>", vim.cmd.vsplit)
+-- vim.keymap.set("n", "<C-S>", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 vim.keymap.set("v", "<S-k>", ":move '<-2<CR>gv-gv")
 vim.keymap.set("v", "<S-j>", ":move '>+1<CR>gv-gv")
@@ -10,3 +10,4 @@ vim.keymap.set("n", "<leader>nh", vim.cmd.nohl)
 vim.keymap.set("n", "<A-1>", ":ToggleTerm direction=horizontal<CR>")
 vim.keymap.set("n", "<A-2>", ":ToggleTerm direction=float<CR>")
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.keymap.set("n", "<leader>nh", ":nohl")
